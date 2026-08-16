7c96af8170ce67d57ff2a7ed060d8866dd5ec03d
Rohan-62 <rohanannadurai18@gmail.com>
2026-08-16 06:23:01 +0530

    Time: 740 ms (5.01%) | Memory: 47.2 MB (5.93%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+    public void dfs(int[] nums,List li,HashSet set){
+        if(set.size()==nums.length){
+            if(!res.contains(li)){
+                res.add(new ArrayList(li));
+                return ;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(i)){
+                set.add(i);
+                li.add(nums[i]);
+                dfs(nums,li,set);
+                li.remove(li.size()-1);
+                set.remove(i);
+            }
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<Integer> li=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            li.add(nums[i]);
+            set.add(i);
+            dfs(nums,li,set);
+            set.remove(i);
+            li.remove(li.size()-1);
+        }
+        return res;
+    }
+}
