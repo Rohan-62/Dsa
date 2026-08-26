b47489cb005b99340148118222377f203e650a46
Rohan-62 <rohanannadurai18@gmail.com>
2026-08-26 11:19:35 +0530

    Time: 1 ms (85.79%) | Memory: 43.9 MB (98.34%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    ArrayList<String>res=new ArrayList<>();
+
+    public void generatepara(int n,StringBuilder sb,int open,int close){
+        if(open>n || close>n){
+            return ;
+        }
+        if(close>open){
+            return;
+        }
+        
+        if(open==n && close==n){
+            res.add(sb.toString());
+        }
+        
+        if(open<n){
+            sb.append("(");
+            generatepara(n,sb,open+1,close);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<n){
+            sb.append(")");
+            generatepara(n,sb,open,close+1);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+
+
+    }
+    public List<String> generateParenthesis(int n) {
+        StringBuilder sb=new StringBuilder("(");
+        generatepara(n,sb,1,0);
+        return res;
+    }
+}
