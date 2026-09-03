817b08a467dccd6fa3110876db078769a2f761ce
Rohan-62 <rohanannadurai18@gmail.com>
2026-09-03 06:52:07 +0530

    Time: 6 ms (56.52%) | Memory: 121.4 MB (83.33%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        HashSet<Integer>set=new HashSet<>();
         int min=Integer.MAX_VALUE;
-
         for(int i=0;i<nums1.length;i++){
             if(nums1[i]<min){
                 min=nums1[i];
@@ -10,22 +8,14 @@ class Solution {
         }
         if(min%2==0){
             for(int i=0;i<nums1.length;i++){
-                if(nums1[i]%2==0){
-                    set.add(nums1[i]);
-                }else{
-                    if((nums1[i]-min)%2==1){
+                if(nums1[i]%2==1 &&(nums1[i]-min)%2==1){
                         return false;
-                    }
                 }
             }
         }else{
             for(int i=0;i<nums1.length;i++){
-                if(nums1[i]%2==1){
-                    set.add(nums1[i]);
-                }else{
-                    if((nums1[i]-min)%2==0){
+                if(nums1[i]%2==0 &&(nums1[i]-min)%2==0){
                         return false;
-                    }
                 }
             }
         }
