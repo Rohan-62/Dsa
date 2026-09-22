ae88479ce71351f54cf07276b56dcb9d3918cb67
Rohan-62 <rohanannadurai18@gmail.com>
2026-09-22 15:53:11 +0530

    Time: 147 ms (38.60%) | Memory: 43.2 MB (28.59%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,49 +1,33 @@
 class Solution {
-    public boolean dfs (char[][]board,int i,int j,String word,StringBuilder sb,boolean[][]check){
-        if(sb.length()==word.length()){
-            String str=sb.toString();
-           
-            if(str.equals(word)){
-                System.out.println(str);
-                return true;
-            }else{
-                return false;
-            }
-        }   
-        
+    public boolean dfs (char[][]board,int i,int j,String word,int idx,boolean[][]check){
+        if(idx>=word.length()){
+            return true;
+        }
         if(i>=board.length ||i<0 ||j>=board[0].length||j<0){
             return false;
         }
-          
-        
-        int len=sb.length();
-        boolean top=false;
-        boolean bottom=false;
-        boolean left=false;
-        boolean right=false;
-         if(check[i][j]==false){
-                     sb.append(board[i][j]);
-                     check[i][j]=true;
-                    top=dfs(board,i-1,j,word,sb,check);
-                    bottom=dfs(board,i+1,j,word,sb,check);
-                    left=dfs(board,i,j-1,word,sb,check);
-                    right=dfs(board,i,j+1,word,sb,check);
-                    check[i][j]=false;
-                    sb.setLength(len);
-                }
-        
-              
-                return top||bottom||left||right;
+        if(word.charAt(idx)!=board[i][j]){
+            return false;
+        }
+        if(check[i][j]==true){
+            return false;
+        }
+            check[i][j]=true;
+            boolean top=dfs(board,i-1,j,word,idx+1,check);
+            boolean bottom=dfs(board,i+1,j,word,idx+1,check);
+            boolean left=dfs(board,i,j-1,word,idx+1,check);
+            boolean right=dfs(board,i,j+1,word,idx+1,check);
+            check[i][j]=false;
+            return top||bottom||left||right;
         
 
     }
     public boolean exist(char[][] board, String word) {
-        StringBuilder sb=new StringBuilder();
         boolean check[][]=new boolean[board.length][board[0].length];
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    boolean res=dfs(board,i,j,word,sb,check);
+                    boolean res=dfs(board,i,j,word,0,check);
                     if(res==true){
                         return true;
                     }
